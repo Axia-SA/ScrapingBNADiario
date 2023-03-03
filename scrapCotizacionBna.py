5a1f913f33d53572e06577ccfcb371e28a300797
Cristian Bottazzi <cristian1604@educ.ar>
2023-03-03 12:49:33 -0300

    Refactorización completa del módulo: Cambio de Scrappy por BeautifulSoup4

--- scrapCotizacionBna.py
+++ scrapCotizacionBna.py
@@ -1,110 +1,127 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
-# -*- coding: utf-8 -*- 
-import scrapy
+# -*- coding: utf-8 -*-
+
+import requests
 import json
-#import requests  <-- Descomentar para enviar los datos obtenidos por POST
+from bs4 import BeautifulSoup
+
 
 class Moneda:
-    compra = ''
-    venta = ''
+    compra = 0
+    venta = 0
     idMoneda = ''
     fecha = ''
-    def __init__(self, idD, c, v, f, nD):
-        self.idMoneda = idD
-        self.compra = c
-        self.venta = v
-        self.fecha = f
-        self.nombreDivisa = nD
+    def __init__(self, idDivisa, valorCompra, valorVenta, fecha, nombreDivisa):
+        self.idMoneda = idDivisa
+        self.compra = valorCompra
+        self.venta = valorVenta
+        self.fecha = fecha
+        self.nombreDivisa = nombreDivisa
 
-# Obtiene de la página del Banco Nación las cotizaciones de las divisas, y la cotización
-# de billetes del Real (dividido por 100).
-# Posteriormente realiza un POST al backend del sistema de Axia para guardar los datos
+def buildClass(nombreDivisa, valorCompra, valorVenta, fecha):
+    idDivisa = 0
+    agregar = 0
+    if nombreDivisa == "Dolar U.S.A":
+        idDivisa = 2
+        nombreDivisa = 'Dolar'
+        agregar = True
+    if nombreDivisa == "Real (*)":
+        idDivisa = 4
+        agregar = True
+        nombreDivisa = 'Real'
+    if nombreDivisa == "Euro":
+        idDivisa = 3
+        agregar = True
+        nombreDivisa = 'Euro'
+    if nombreDivisa == "Libra Esterlina":
+        idDivisa = 5
+        agregar = True
+        nombreDivisa = 'Libra Esterlina'
+    if nombreDivisa == "Real *":
+        idDivisa = 4
+        agregar = True
+        nombreDivisa = 'Real'
+    if (agregar is True):
+        return Moneda(idDivisa, valorCompra, valorVenta, fecha, nombreDivisa)
+    else:
+        return None
 
-class BootstrapTableSpider(scrapy.Spider):
-    name = "tabla"
+def postDataToServer(vectorMoneda):
+     for row in vectorMoneda:
+        s = json.dumps(row.__dict__)
+        print('DATA -> ' + s)
+        headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+        # Completar la URL de envío
+        # r = requests.post("__my_url__", data=s, headers=headers)
+        # print(r.status_code, r.reason)
 
-    def start_requests(self):
-        urls = [
-            'https://www.bna.com.ar',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+# Realizar solicitud GET a la URL
+url = "https://www.bna.com.ar/Personas"
+response = requests.get(url)
 
-    def parse(self, response):
-        fecha = ''
-        m = []
+# Analizar el contenido HTML de la respuesta
+soup = BeautifulSoup(response.content, 'html.parser')
 
-        for row in response.xpath('//*[@id="divisas"]//thead/tr'):
-            fecha = row.xpath('th[1]//text()').extract_first()
+# Encontrar la tabla de cotizaciones
+table_divisa = soup.find('div', {'id': 'divisas'})
+table_billete = soup.find('div', {'id': 'billetes'})
 
-        for row in response.xpath('//*[@id="divisas"]//tbody/tr'):
-            divisa = row.xpath('td[1]//text()').extract_first()
-            compra = row.xpath('td[2]//text()').extract_first()
-            venta = row.xpath('td[3]//text()').extract_first()
-            compra = compra.replace(',', '.')
-            venta = venta.replace(',', '.')
+# Obtener la segunda fila de la tabla
+rows = table_divisa.find_all('tr')
+row2 = rows[1]
+row3 = rows[2]
+row4 = rows[3]
 
-            idDivisa = 0
-            agregar = 0
-            nombreDivisa = ''
-            if divisa == "Dolar U.S.A":
-                idDivisa = 2
-                nombreDivisa = 'Dolar'
-                agregar = 1
-            if divisa == "Real (*)":
-                idDivisa = 4
-                agregar = 1
-                nombreDivisa = 'Real'
-                compra = float(compra)/100
-                venta = float(venta)/100
-            if divisa == "Euro":
-                idDivisa = 3
-                agregar = 1
-                nombreDivisa = 'Euro'
-            if divisa == "Libra Esterlina":
-                idDivisa = 5
-                agregar = 1
-                nombreDivisa = 'Libra Esterlina'
+rows_billete = table_billete.find_all('tr')
+row_billete = rows_billete[3]
 
-            if agregar:
-                tmp = Moneda(idDivisa, float(compra), float(venta), fecha, nombreDivisa)
-                m.append(tmp)
+# Obtener el valor de las filas
+column1_usd = row2.find_all('td')[0].text
+column2_usd = row2.find_all('td')[1].text.replace(",",".")
+column3_usd = row2.find_all('td')[2].text.replace(",",".")
 
+column1_eur = row3.find_all('td')[0].text
+column2_eur = row3.find_all('td')[1].text.replace(",",".")
+column3_eur = row3.find_all('td')[2].text.replace(",",".")
 
-        for row in response.xpath('//*[@id="billetes"]//thead/tr'):
-            fecha = row.xpath('th[1]//text()').extract_first()
+column1_gbp = row4.find_all('td')[0].text
+column2_gbp = row3.find_all('td')[1].text.replace(",",".")
+column3_gbp = row4.find_all('td')[2].text.replace(",",".")
 
-        # Como la cotizacion de divisas no posee el Real, este se obtiene de la cotizacion de billetes
-        for row in response.xpath('//*[@id="billetes"]//tbody/tr'):
-            divisa = row.xpath('td[1]//text()').extract_first()
-            compra = row.xpath('td[2]//text()').extract_first()
-            venta = row.xpath('td[3]//text()').extract_first()
-            compra = compra.replace(',', '.')
-            venta = venta.replace(',', '.')
+column1_bra = row_billete.find_all('td')[0].text
+column2_bra = row_billete.find_all('td')[1].text.replace(",",".")
+column3_bra = row_billete.find_all('td')[2].text.replace(",",".")
 
-            idDivisa = 0
-            agregar = 0
-            if divisa == "Real *":
-                idDivisa = 4
-                agregar = 1
-                nombreDivisa = 'Real'
-                compra = float(compra)/100
-                venta = float(venta)/100
+# Obtener valor de fecha D/M/YYYY
+fecha_cot = soup.find('th', {'class': 'fechaCot'}).text
+# Convertir a formato de fecha ISO-8601
+fecha_cot = fecha_cot.split("/")[2].zfill(2) + "-" + fecha_cot.split("/")[1].zfill(2) + "-" + fecha_cot.split("/")[0].zfill(2)
+print("Fecha: ", fecha_cot)
 
-            if agregar:
-                tmp = Moneda(idDivisa, float(compra), float(venta), fecha, nombreDivisa)
-                m.append(tmp)
+vectDivisa = []
+# Imprimir los valores obtenidos y agregar los valores a un vector de Moneda()
+print(column1_usd, " : ", column2_usd, " : ", column3_usd)
+res = buildClass(column1_usd, column2_usd, column3_usd, fecha_cot)
+if (res is not None):
+    vectDivisa.append(res)
 
+print(column1_eur, " : ", column2_eur, " : ", column3_eur)
+res = buildClass(column1_eur, column2_eur, column3_eur, fecha_cot)
+if (res is not None):
+    vectDivisa.append(res)
 
-        for row in m:
-            # Convertir fecha del tipo d/m/yyyy al tipo ISO 8601 yyyy-mm-dd
-            row.fecha = row.fecha.split("/")[2].zfill(2) + "-" + row.fecha.split("/")[1].zfill(2) + "-" + row.fecha.split("/")[0].zfill(2)
-            
-            s = json.dumps(row.__dict__)
-            print('DATA -> ' + s)
-            # Enviar los datos obtenidos por POST al backend de un sistema
-            # headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}  <- Cabeceras
-            # r = requests.post("http://url.com/backend/my_endpoint", data=s, headers=headers)
-            # print(r.status_code, r.reason)
+print(column1_gbp, " : ", column2_gbp, " : ", column3_gbp)
+res = buildClass(column1_gbp, column2_gbp, column3_gbp, fecha_cot)
+if (res is not None):
+    vectDivisa.append(res)
 
+print(column1_bra, " : ", float(column2_bra)/100, " : ", float(column3_bra)/100)
+res = buildClass(column1_bra, float(column2_bra)/100, float(column3_bra)/100, fecha_cot)
+if (res is not None):
+    vectDivisa.append(res)
+
+# print("=================================================")
+# print("    POST DATA    ")
+# print("=================================================")
+# postDataToServer(vectDivisa)
